8f11a35c0ae39e7a3d3dc28a6adacbdec261945b
Elliot Smith <elliot@elliotcsmith.com>
2025-12-03 05:44:49 +1000

    day 2 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use core::panic;
 use std::{
     collections::{HashMap, HashSet},
-    fs,
+    fs, usize,
 };
 
 use anyhow::{Context, Result};
@@ -70,6 +70,58 @@ pub fn day01(filename: String, part_b: bool) -> Result<()> {
     Ok(())
 }
 
+#[instrument]
+pub fn day02(filename: String, part_b: bool) -> Result<()> {
+    let content = fs::read_to_string(filename).context("Couldn't read input")?;
+
+    // invalid IDs are the same sequence repeated twice.
+    // For an integer of length N (where N is even), split N into 2 parts
+    // There will be 1 invlid integer for each value of half(N) in the range
+    // log_10(i) will give the length, log_10(min) log_10(max) will give the bounds
+    // Can also just use the string length
+    // if log_10(x) % 2 == 0 -> for v in x -> x*10 - 1 -> if v + v in range -> add v + v (str wise)
+    // then increment x to 100*x
+
+    let mut total = 0;
+    let clean = content.replace('\n', "");
+    for range in clean.split(',') {
+        //println!("{:?}", range);
+        let (r_low, r_high) = range.split_once('-').unwrap();
+        let mut x = r_low.parse::<i64>().unwrap();
+        let r_low = r_low.parse::<i64>().unwrap();
+        let r_high = r_high.parse::<i64>().unwrap();
+
+        while x <= r_high {
+            let digits = x.to_string().len();
+            if digits % 2 == 0 {
+                let half = (digits / 2) as usize;
+                let half_digit = x.to_string()[..half].parse::<i64>().unwrap();
+                let mut h: i64 = half_digit;
+                let upper = 10i64.pow(half as u32);
+                //println!("{:?} -> upper", upper);
+                while h < upper {
+                    //println!("{:?} -> h", h);
+                    let test = h + h * 10i64.pow(half as u32);
+                    if test >= r_low {
+                        if test > r_high {
+                            break;
+                        }
+                        //println!("{:?} -> test", test);
+                        total += test;
+                    }
+                    h += 1;
+                }
+            }
+            x = 10i64.pow(digits as u32);
+        }
+    }
+
+    println!("{:?}", total);
+
+    Ok(())
+}
+
 fn main() {
-    day01("./inputs/day01a.txt".to_string(), true);
+    // day01("./inputs/day01a.txt".to_string(), true);
+    day02("./inputs/day02a.txt".to_string(), false);
 }
